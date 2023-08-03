fae42f549ab5b998ac9fdfd23f105d8f03fdd1b1
locoioioi <118866346+locoioioi@users.noreply.github.com>
2023-08-03 09:14:31 +0700

    login

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,12 +1,39 @@
 var register = document.querySelector('.nav__register');
-var modalRegister = document.querySelector('.modal__register-display');
-var modalRegisterRemove = document.querySelector('.modal__register-remove');
+var login = document.querySelector('.nav__login');
+var modal = document.querySelector('.modal__display');
+var modalRemove = document.querySelector('.modal__remove');
+var registerModal    = document.querySelector('.auth-form__register');
+var loginModal = document.querySelector('.auth-form__login');
 
-// open register
+// open modal
 register.addEventListener('click', function () {
-    modalRegister.classList.add('modal-display');
+    modal.classList.add('modal-display');
+    registerOpen()
 });
+login.addEventListener('click', function () { 
+    modal.classList.add('modal-display');
+    loginOpen()
+});
+// close modal
+modalRemove.addEventListener('click', function () {
+    modal.classList.remove('modal-display');
+    registerRemove();
+    loginRemove();
+});
+// open register
+function registerOpen() {
+    registerModal.classList.remove('auth-form__register');
+}
 // close register
-modalRegisterRemove.addEventListener('click', function () {
-    modalRegister.classList.remove('modal-display');
-});
+function registerRemove() { 
+    registerModal.classList.add('auth-form__register');
+}
+
+// open login
+function loginOpen() {
+    loginModal.classList.remove('auth-form__login');
+}
+// close login
+function loginRemove() { 
+    loginModal.classList.add('auth-form__login');
+}
