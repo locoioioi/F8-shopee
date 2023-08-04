d463f1830bfcf41cbb8ef77a8642ab1c89966d8a
locoioioi <118866346+locoioioi@users.noreply.github.com>
2023-08-04 10:13:55 +0700

    hi

--- assests/js/script.js
+++ assests/js/script.js
@@ -4,6 +4,8 @@ var modal = document.querySelector('.modal__display');
 var modalRemove = document.querySelector('.modal__remove');
 var registerModal    = document.querySelector('.auth-form__register');
 var loginModal = document.querySelector('.auth-form__login');
+var switchRegister = document.querySelector('.switch-btn__register');
+var switchLogin = document.querySelector('.switch-btn__login');
 
 // open modal
 register.addEventListener('click', function () {
@@ -19,6 +21,16 @@ modalRemove.addEventListener('click', function () {
     modal.classList.remove('modal-display');
     registerRemove();
     loginRemove();
+});
+// switch register
+switchRegister.addEventListener('click', function () { 
+    registerOpen();
+    loginRemove();
+});
+// switch login
+switchLogin.addEventListener('click', function () {
+    loginOpen();
+    registerRemove();
 });
 // open register
 function registerOpen() {
